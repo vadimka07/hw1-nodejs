a17fd5296adc0c95ed06b77d2657c73f648b5b75
Vadim <vadger@yahoo.com>
2023-07-11 19:09:11 +0300

    add hw01

--- contacts.js
+++ contacts.js
@@ -1,15 +1,15 @@
 import fs from "fs/promises";
-import contacts from "./db/contacts.json";
 import path from "path";
 import {nanoid} from "nanoid";
 
-const contactsPath = path.basename(contacts);
 
-function listContacts() {
+const contactsPath = path.resolve('db/contacts.json');
+async function listContacts() {
   // ...твій код. Повертає масив контактів.
-  fs.readFile(contacts)
+  await fs
+    .readFile(contactsPath)
     .then((data) => {
-      console.log(data);
+      console.table(JSON.parse(data.toString()));
     })
     .catch((error) => {
       console.log(error);
@@ -17,43 +17,50 @@ function listContacts() {
 }
 
 function getContactById(contactId) {
-  return fs.readFile(contacts).then((data) => {
-    const result = data.find((item) => item.id === contactId);
-    if (result.id !== contactId) {
-      return null;
+  return fs.readFile(contactsPath).then((data) => {
+    const result = JSON.parse(data).find((item) => item.id === contactId);
+    if (!result) {
+      return console.log(null);
     }
-    return result;
+    return console.log(result);
   });
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
 }
 
 async function removeContact(contactId) {
   const contact = await getContactById(contactId);
   if (!contact) {
-    return null;
+    return console.log(null);
   }
-  const allContacts = await fs.readFile(contacts);
+  const allContacts = await fs.readFile(contactsPath);
   const normilizeData = JSON.parse(allContacts.toString());
   const result = normilizeData.filter((item) => item.id !== contact.id);
   await fs.writeFile(contactsPath, JSON.stringify(result));
-  return contact;
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  return console.log(contact);
 }
 
 async function addContact(name, email, phone) {
-  await fs.appendFile(contacts, { name, email, phone }.toString());
-  return {
+  const allContacts = await fs.readFile(contactsPath);
+  const normilizeData = JSON.parse(allContacts.toString());
+  const contact = {
     id: nanoid(),
     name,
     email,
     phone,
   };
-  // ...твій код. Повертає об'єкт доданого контакту.
+  const result = normilizeData.find((item) => item.id === contact.id);
+  if(result){
+    return console.log('This id is true')
+  }
+  normilizeData.push(contact);
+  await fs.writeFile(contactsPath, JSON.stringify(normilizeData));
+  return console.log(contact);
 }
 
-module.exports = {
+export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
 };
+
+
--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import contacts from "./contacts.js";
 
 
-const argv = yargs.argv;
-function invokeAction({ action, id, name, email, phone }) {
-    switch (action) {
-        case 'list':
-            // ...
-            break;
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      await contacts.listContacts();
+      break;
 
-        case 'get':
-            // ... id
-            break;
+    case "get":
+      await contacts.getContactById(id);
+      break;
 
-        case 'add':
-            // ... name email phone
-            break;
+    case "add":
+      await contacts.addContact(name, email, phone);
+      break;
 
-        case 'remove':
-            // ... id
-            break;
+    case "remove":
+      await contacts.removeContact(id);
+      break;
 
-        default:
-            console.warn('\x1B[31m Unknown action type!');
-    }
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
 }
 
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
 invokeAction(argv);
+
